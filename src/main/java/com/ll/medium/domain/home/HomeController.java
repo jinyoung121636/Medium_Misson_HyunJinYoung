ffae7c0ec297a982b2292180854083b3648ca585
jinyoung121636 <damin0518@hanmail.net>
2023-12-12 03:50:35 +0900

    로그인&로그아웃 구현

--- src/main/java/com/ll/medium/domain/home/HomeController.java
+++ src/main/java/com/ll/medium/domain/home/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.medium.domain.home;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
--- src/main/java/com/ll/medium/domain/member/MemberSecurityService.java
+++ src/main/java/com/ll/medium/domain/member/MemberSecurityService.java
@@ -1,5 +1,41 @@
+// 해당 아이디에 저장된 비밀번호를 찾는것
+
 package com.ll.medium.domain.member;
 
 
-public class MemberSecurityService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberSecurityService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        Optional<SiteMember>_siteMember = this.memberRepository.findByusername(username);
+        if(_siteMember.isEmpty()){
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+        }
+        SiteMember siteMember = _siteMember.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if("admin".equals(username)){
+            authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
+        }else {
+            authorities.add(new SimpleGrantedAuthority((MemberRole.USER.getValue())));
+        }
+        return new User(siteMember.getUsername(), siteMember.getPassword(), authorities);
+    }
 }
